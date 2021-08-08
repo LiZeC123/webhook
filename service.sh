0730fd43413d34a6dcc29011ac9801cc6f4b62e4
LiZeC <tomgreem123@gmail.com>
2021-08-08 22:01:26 +0800

    添加控制脚本

---
+++ service.sh
@@ -0,0 +1,45 @@
+function compileService() {
+  go build
+}
+
+function runService() {
+  nohup ./webhook > log.txt 2>&1 &
+}
+
+function stopService() {
+  pid=pgrep "webhook"
+  kill "$pid"
+}
+
+function backup() {
+  zip -r webhook.zip command/ config.josn
+}
+
+if [ "$1"x == "start"x ]; then
+  compileService
+  runService
+elif [ "$1"x == "compile"x ]; then
+  compileService
+elif [ "$1"x == "run"x ]; then
+  runService
+elif [ "$1"x == "stop"x ]; then
+  stopService
+elif [ "$1"x == "restart"x ]; then
+  stopService
+  compileService
+  runService
+elif [ "$1"x == "backup"x ]; then
+  backup
+else
+  echo "无效的参数: $1"
+  echo ""
+  echo "用法: ./service [参数]"
+  echo "参数可以选择以下值:"
+  echo "start     编译并启动项目"
+  echo "stop      停止项目"
+  echo "restart   重启项目"
+  echo "compile   只编译项目"
+  echo "run       直接运行项目"
+  echo "backup    备份数据文件"
+  echo ""
+fi
