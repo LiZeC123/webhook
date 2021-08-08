30e792f450c857b3a67108edbd969cef85795d05
LiZeC <tomgreem123@gmail.com>
2021-08-08 21:22:08 +0800

    clear

--- main.go
+++ main.go
@@ -2,23 +2,20 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 	"os/exec"
 	"strings"
-	"time"
 )
 
-const (
-	TYPE_GITHUB string = "GithubHook"
-	TYPE_USER   string = "UserHook"
+// GithubHook UserHook
 
-	STATUS_DONE string = "Done"
-	STATUS_RUN  string = "Running"
-)
+type Config struct {
+	Token  string
+	Config []AppConfig
+}
 
 type AppConfig struct {
 	AppName string
@@ -27,19 +24,11 @@ type AppConfig struct {
 	Cmd     []string
 }
 
-type AppStatus struct {
-	AppName string
-	Type    string
-	Status  string
-	Time    string
-}
-
-var configs []AppConfig
-var status = make(map[string]*AppStatus)
+var configs Config
 
 func main() {
 	loadConfig()
-	initStatus()
+
 	// handler是异步执行的
 	http.HandleFunc("/", handleWebHook)
 
@@ -71,74 +60,35 @@ func loadConfig() {
 	log.Printf("Load Config: %v\n", configs)
 }
 
-func initStatus() {
-	for _, config := range configs {
-		status[config.AppName] = &AppStatus{AppName: config.AppName, Type: config.Type, Status: STATUS_DONE, Time: nowString()}
-	}
-}
-
-func handleWebHook(writer http.ResponseWriter, request *http.Request) {
+func handleWebHook(_ http.ResponseWriter, request *http.Request) {
 	var URI = request.URL.RequestURI()
-	log.Println("Receive Request: " + URI)
-	if URI == "/" {
-		writeIndexFile(writer, request)
-		return
-	}
+	log.Println("接受请求: " + URI)
 
 	value := strings.Split(URI, "/")
-	// "/A/B" -> ["", "A", "B"]
-	if len(value) != 3 {
-		writeError(writer, request, "参数数量不正确")
+	// "/Token/Type/AppName" -> ["", "Token", "Type", "AppName"]
+	if len(value) != 4 {
+		log.Print("参数数量不正确, 忽略请求")
 		return
 	}
 
-	var appType = value[1]
-	var appName = value[2]
-	for _, config := range configs {
+	var token = value[1]
+	if token != configs.Token {
+		log.Print("Token错误, 忽略请求")
+		return
+	}
+
+	var appType = value[2]
+	var appName = value[3]
+	for _, config := range configs.Config {
 		if config.AppName == appName && config.Type == appType {
-			appStatus := status[config.AppName]
-			setRunning(appStatus)
+			log.Printf("开始执行请求 -->  App:%s Type:%s", appName, appType)
 			execShell(config.WorkDir, config.Cmd)
-			setDone(appStatus)
-			writeDone(writer)
+			log.Printf("请求执行结束")
 			return
 		}
 	}
 
-	msg := fmt.Sprintf("Undefined Request: appName= %s appType=%s", appName, appType)
-	writeError(writer, request, msg)
-}
-
-func writeIndexFile(w http.ResponseWriter, r *http.Request) {
-	var html = "<table border=\"1\">" +
-		"<tr><th>AppName</th><th>Status</th><th>Operation</th><th>Update Time</th></tr>"
-
-	// _, _ = fmt.Fprintf(w, "%-15s\t%-10s\t%-15s\n", "AppName", "Status", "Time")
-
-	var row = make([]string, len(status))
-	for _, s := range status {
-		var template = "<tr><td>#AppName#</td><td>#AppStatus#</td><td>#Action#</td><td>#Time#</td></tr>"
-		template = strings.Replace(template, "#AppName#", s.AppName, 1)
-		template = strings.Replace(template, "#AppStatus#", s.Status, 1)
-		var action = "<a href ='#path#' target='_Blank'>Trigger</a>"
-		action = strings.Replace(action, "#path#", "/"+s.Type+"/"+s.AppName, 1)
-		template = strings.Replace(template, "#Action#", action, 1)
-		template = strings.Replace(template, "#Time#", s.Time, 1)
-		row = append(row, template)
-	}
-
-	html = html + strings.Join(row, "\n") + "</table>"
-
-	_, _ = fmt.Fprint(w, html)
-
-}
-
-func writeError(w http.ResponseWriter, r *http.Request, msg string) {
-	_, _ = fmt.Fprintf(w, msg)
-}
-
-func writeDone(w http.ResponseWriter) {
-	_, _ = fmt.Fprintf(w, "Done")
+	log.Printf("未注册的操作 --> App:%s Type:%s", appName, appType)
 }
 
 func execShell(workDir string, cmd []string) {
@@ -146,21 +96,7 @@ func execShell(workDir string, cmd []string) {
 	for i := 0; i < len(cmd); i++ {
 		fullCommand = fullCommand + cmd[i] + ";"
 	}
-	log.Printf("Do Command: %s", fullCommand)
+	log.Printf("执行指令: %s", fullCommand)
 
 	_ = exec.Command("bash", "-c", fullCommand).Run()
-}
-
-func nowString() string {
-	return time.Now().Format("2006-01-02 15:04:05")
-}
-
-func setRunning(appStatus *AppStatus) {
-	appStatus.Status = STATUS_RUN
-	appStatus.Time = nowString()
-}
-
-func setDone(appStatus *AppStatus) {
-	appStatus.Status = STATUS_DONE
-	appStatus.Time = nowString()
 }
